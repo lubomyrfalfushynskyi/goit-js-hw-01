00fed4806cc1837956687362c215f9905cf14640
Admin <lubomyrfalfushinskyi@gmail.com>
2025-09-15 22:54:28 +0300

    ver_1 - init

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
-function makeTransaction(quantity, priceForDroid){
-    totalPrice = pricePerDroid*quantity;
-    return `You ordered ${quantity} droids worth <totalPrice> credits!`
+function makeTransaction(quantity, pricePerDroid){
+    let totalPrice = pricePerDroid*quantity;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-function makeTransaction(quantity, priceForDroid){
-    //...
+function getElementWidth(content, padding, border){
+    return content + 2*(padding + border);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
